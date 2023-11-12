aed7395d6600f9364c05b8434a2adb81478b9735
Kenny <kenny@knnyczr.com>
2023-11-12 15:53:50 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `theprojectlead`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
